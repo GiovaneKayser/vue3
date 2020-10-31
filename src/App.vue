1aed1b9ef2c91964ba6c24ee77470b38938e4411
GiovaneKayser <giovane.kayser16@gmail.com>
2020-10-31 20:00:23 -0300

    primeiro envio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>DC heroes</h1>
+  <ul>
+     <li v-for="(hero,index) in dcHeros" v-bind:key="index">{{hero.name}}</li>
+  </ul>
+  <!-- <input v-model.lazy="newHero"/> -->
+  <form @submit.prevent="addHero">
+  <input v-model="newHero" placeholder="type hero name here"/>
+  <button type="submit">Add hero</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return {
+
+            newHero:"",
+            dcHeros:[
+                {name:'SuperGirl'},
+                {name:'Flash'},
+                {name:'Batman'},
+                {name:'Arrow'},
+                {name:'SuperMan'}
+                ]
+        }
+    },
+    methods:{
+        addHero(){
+            if (this.newHero !== ""){
+                this.dcHeros.push({name: this.newHero}); this.newHero=''
+            }
+            
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
