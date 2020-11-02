1a2b5abf77d212d1222f8de64a63d5bf7c98f0e1
GiovaneKayser <giovane.kayser16@gmail.com>
2020-11-01 22:55:29 -0300

    Adição do carousel

--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -3,7 +3,7 @@
     <h1 class="w-full text-center text-3xl my-4" >Markdown App</h1>
   <section class="flex m-auto w-10/12 h-screen">
       <article class="w-1/2 border">
-      <textarea class="w-full h-full" v-model="text"></textarea></article>
+      <textarea class="w-full h-full" :value="text" @input="update"></textarea></article>
       <article class="w-1/2 border bg-gray-100" v-html="markedText"></article>
   </section>
 </div>
@@ -11,16 +11,27 @@
 
 <script>
 import marked from "marked";
+import debounce from "../utilities/mixins/debounce"
 export default {
+    
+    mixins:[debounce],
     data(){
         return{
-            text:""
+            text:"",
         }
     },
     computed:{
         markedText(){
             return marked(this.text);
         }
+    },
+    methods:{
+        //atualizar depois de 500ms
+        update(e){
+           const task = ()=> (this.text = e.target.value)
+           this.debounce(task, 500)
+           
+        },   
     }
 }
 </script>
